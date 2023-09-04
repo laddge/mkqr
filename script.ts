fcc0beb1ba9e231ba27b9ce978bb4cc9f0500313
Laddge <dev.laddge@gmail.com>
2023-09-04 00:33:06 +0000

    update

--- script.ts
+++ script.ts
@@ -10,6 +10,19 @@ toDataURL(location.href)
     s.top = '0px'
     s.left = '0px'
     s.backgroundColor = 'rgba(0, 0, 0, .5)'
+    s.zIndex = '2147483647'
+    s.padding = '16px'
+    s.textAlign = 'center'
+    w.onclick = () => {
+      w.remove()
+    }
+    const i = document.createElement('img')
+    const S = i.style
+    S.width = '480px'
+    S.maxWidth = '100%'
+    S.aspectRatio = '1'
+    i.src = url
+    w.appendChild(i)
     document.body.appendChild(w)
   })
   .catch(e => console.log(e))
